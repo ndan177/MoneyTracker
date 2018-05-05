351c3bab45f970a07aa674442318e233ca60cd9a
Daniel <nohaidany96@gmail.com>
2018-05-05 16:58:40 +0300

    tested contacts

--- app/src/main/java/com/example/nohai/moneytracker/UI/BorrowTo.java
+++ app/src/main/java/com/example/nohai/moneytracker/UI/BorrowTo.java
@@ -1,14 +1,88 @@
 package com.example.nohai.moneytracker.UI;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.provider.ContactsContract;
+import android.support.annotation.NonNull;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.provider.ContactsContract.Contacts;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
 
 import com.example.nohai.moneytracker.R;
 
+
+
 public class BorrowTo extends AppCompatActivity {
+    private static final int READ_CONTACTS_PERMISSIONS_REQUEST = 1;
+    private static final int CONTACT_PICKER_RESULT = 1001;
+    private static String DEBUG_TAG = "My App Logging";
+
+    public void doLaunchContactPicker(View view) {
+        Intent contactPickerIntent = new Intent(Intent.ACTION_PICK,
+                Contacts.CONTENT_URI);
+        startActivityForResult(contactPickerIntent, CONTACT_PICKER_RESULT);
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode == RESULT_OK) {
+            switch (requestCode) {
+                case CONTACT_PICKER_RESULT:
+                    Cursor cursor = null;
+                    String phone = "";
+                    try {
+                        Uri result = data.getData();
+                        Log.v(DEBUG_TAG, "Got a contact result: "
+                                + result.toString());
+
+                        // get the contact id from the Uri
+                        String id = result.getLastPathSegment();
+                        getPermissionToReadUserContacts();
+                        // query for everything phone
+                        cursor = getContentResolver().query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
+                                null, ContactsContract.CommonDataKinds.Phone.CONTACT_ID + "=?", new String[] { id },
+                                null);
+
+                        int phoneIdx = cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME);
+
+                        // let's just get the first phone
+                        if (cursor.moveToFirst()) {
+                            phone = cursor.getString(phoneIdx);
+                            Log.v(DEBUG_TAG, "Got phone: " + phone);
+                        } else {
+                            Log.w(DEBUG_TAG, "No results");
+                        }
+                    } catch (Exception e) {
+                        Log.e(DEBUG_TAG, "Failed to get phone data", e);
+                    } finally {
+                        if (cursor != null) {
+                            cursor.close();
+                        }
+                        TextView phoneEntry = findViewById(R.id.person);
+                        phoneEntry.setText(phone);
+                        if (phone.length() == 0) {
+                            Toast.makeText(this, "No phone found for contact.",
+                                    Toast.LENGTH_LONG).show();
+                        }
+
+                    }
+
+                    break;
+            }
+
+        } else {
+            Log.w(DEBUG_TAG, "Warning: activity result not ok");
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +92,7 @@ public class BorrowTo extends AppCompatActivity {
 
     }
 
+
     void setToolbar()
     {
         android.support.v7.widget.Toolbar toolbar = findViewById(R.id.toolbar);
@@ -26,4 +101,48 @@ public class BorrowTo extends AppCompatActivity {
         actionbar.setTitle("Borrow to");
         actionbar.setDisplayHomeAsUpEnabled(true);
     }
+    // user's contacts
+    public void getPermissionToReadUserContacts() {
+        // 1) Use the support library version ContextCompat.checkSelfPermission(...) to avoid
+        // checking the build version since Context.checkSelfPermission(...) is only available
+        // in Marshmallow
+        // 2) Always check for permission (even if permission has already been granted)
+        // since the user can revoke permissions at any time through Settings
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_CONTACTS)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            // The permission is NOT already granted.
+            // Check if the user has been asked about this permission already and denied
+            // it. If so, we want to give more explanation about why the permission is needed.
+            if (shouldShowRequestPermissionRationale(
+                    Manifest.permission.READ_CONTACTS)) {
+                // Show our own UI to explain to the user why we need to read the contacts
+                // before actually requesting the permission and showing the default UI
+            }
+
+            // Fire off an async request to actually get the permission
+            // This will show the standard permission request dialog UI
+            requestPermissions(new String[]{Manifest.permission.READ_CONTACTS},
+                    READ_CONTACTS_PERMISSIONS_REQUEST);
+        }
+    }
+
+    // Callback with the request from calling requestPermissions(...)
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           @NonNull String permissions[],
+                                           @NonNull int[] grantResults) {
+        // Make sure it's our original READ_CONTACTS request
+        if (requestCode == READ_CONTACTS_PERMISSIONS_REQUEST) {
+            if (grantResults.length == 1 &&
+                    grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(this, "Read Contacts permission granted", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(this, "Read Contacts permission denied", Toast.LENGTH_SHORT).show();
+            }
+        } else {
+            super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+
 }
